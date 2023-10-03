ca9684cfd92731a0a908642de7f9fd45f71d1b9e
Joseph <josephylee72@gmail.com>
2023-10-02 17:28:13 -0700

    completed list and keys lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let techMap = technologies.map(item => {
+    return <span key={item.slice(3)}>{item}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techMap}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,13 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const mapped = projects.map(proj => {
+  return <ProjectItem name={proj.name} about={proj.about} technologies={proj.technologies} key={proj.name.slice(3)} />
+})
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{mapped}</div>
     </div>
   );
 }
